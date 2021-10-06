3156a8b41d8c300be84732e78ccc71e5673d0432
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-06 15:50:54 +0200

    feat: add helper functions to convert in32 to string and back

---
+++ taikun/helper.go
@@ -0,0 +1,15 @@
+package taikun
+
+import "strconv"
+
+func atoi32(str string) (int32, error) {
+	res, err := strconv.ParseInt(str, 10, 32)
+	if err != nil {
+		return 0, err
+	}
+	return int32(res), nil
+}
+
+func i32toa(x int32) string {
+	return strconv.FormatInt(int64(x), 10)
+}
