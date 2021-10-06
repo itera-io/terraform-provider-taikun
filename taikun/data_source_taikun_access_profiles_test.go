7a7303e8fbc2c009b67813b422ac7432524b0c9e
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-06 12:34:35 +0200

    test(access_profiles): add access_profiles datasource acceptance test

---
+++ taikun/data_source_taikun_access_profiles_test.go
@@ -0,0 +1,47 @@
+package taikun
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestAccDataSourceTaikunAccessProfiles(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCheckTaikunAccessProfilesConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					func(state *terraform.State) error {
+						fmt.Print(state.String())
+						return nil
+					},
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.#"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "organization_id"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.0.created_by"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.0.dns_servers.#"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.0.id"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.0.is_locked"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.0.last_modified"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.0.last_modified_by"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.0.name"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.0.ntp_servers.#"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.0.organization_id"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.0.organization_name"),
+					resource.TestCheckResourceAttrSet("data.taikun_access_profiles.all", "access_profiles.0.projects.#"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckTaikunAccessProfilesConfig() string {
+	return fmt.Sprintln(`
+data "taikun_access_profiles" "all" {
+  organization_id=441
+}`)
+}
