4d390deb147b0b805a2535a418704ac81698ddf5
Antoine <antoine.labarussias@epita.fr>
2021-10-06 10:50:13 +0200

    feat: setup provider_test.go

--- taikun/provider_test.go
+++ taikun/provider_test.go
@@ -1,20 +1,31 @@
 package taikun
 
 import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"os"
 	"testing"
 )
 
 // providerFactories are used to instantiate a provider during acceptance testing.
 // The factory function will be invoked for every Terraform CLI command executed
 // to create a provider server to which the CLI can reattach.
-// var providerFactories = map[string]func() (*schema.Provider, error){
-// 	"scaffolding": func() (*schema.Provider, error) {
-// 		return New("dev")(), nil
-// 	},
-// }
+var providerFactories = map[string]func() (*schema.Provider, error){
+	"taikun": func() (*schema.Provider, error) {
+		return Provider(), nil
+	},
+}
 
 func TestProvider(t *testing.T) {
 	if err := Provider().InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
+	}
+}
+
+func testAccPreCheck(t *testing.T) {
+	if err := os.Getenv("TAIKUN_USERNAME"); err == "" {
+		t.Fatal("TAIKUN_USERNAME must be set for acceptance tests")
+	}
+	if err := os.Getenv("TAIKUN_PASSWORD"); err == "" {
+		t.Fatal("TAIKUN_PASSWORD must be set for acceptance tests")
 	}
 }
