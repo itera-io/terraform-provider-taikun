54ccacd464bdbec7526bc536ee56e5c9554a41b0
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-07 12:36:10 +0200

    feat(organizations): define organization resource's read method

--- taikun/resource_taikun_organization.go
+++ taikun/resource_taikun_organization.go
@@ -3,9 +3,10 @@ package taikun
 import (
 	"context"
 
-	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+	"github.com/itera-io/taikungoclient/client/organizations"
 )
 
 func resourceTaikunOrganization() *schema.Resource {
@@ -42,9 +43,9 @@ func resourceTaikunOrganization() *schema.Resource {
 				Computed: true,
 			},
 			"discount_rate": {
-				Type:             schema.TypeFloat,
-				Required:         true,
-				ValidateDiagFunc: validation.FloatBetween(0, 100),
+				Type:         schema.TypeFloat,
+				Required:     true,
+				ValidateFunc: validation.FloatBetween(0, 100),
 			},
 			"email": {
 				Type:     schema.TypeString,
@@ -58,6 +59,11 @@ func resourceTaikunOrganization() *schema.Resource {
 				Type:     schema.TypeString,
 				Computed: true,
 			},
+			"let_managers_change_subscription": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
 			"is_locked": {
 				Type:     schema.TypeString,
 				Computed: true,
@@ -65,11 +71,6 @@ func resourceTaikunOrganization() *schema.Resource {
 			"is_read_only": {
 				Type:     schema.TypeString,
 				Computed: true,
-			},
-			"let_managers_change_subscription": {
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  true,
 			},
 			"name": {
 				Type:     schema.TypeString,
@@ -109,6 +110,86 @@ func resourceTaikunOrganization() *schema.Resource {
 }
 
 func resourceTaikunOrganizationRead(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	apiClient := meta.(*apiClient)
+	id, err := atoi32(data.Id())
+	data.SetId("")
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	response, err := apiClient.client.Organizations.OrganizationsDetails(organizations.NewOrganizationsDetailsParams().WithV(ApiVersion).WithOrganizationID(id), apiClient)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	rawOrganization := response.GetPayload().Data
+
+	if err := data.Set("address", rawOrganization.Address); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("billing_email", rawOrganization.BillingEmail); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("city", rawOrganization.City); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("cloud_credentials", rawOrganization.CloudCredentials); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("country", rawOrganization.Country); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("created_at", rawOrganization.CreatedAt); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("discount_rate", rawOrganization.DiscountRate); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("email", rawOrganization.Email); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("full_name", rawOrganization.FullName); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("id", i32toa(rawOrganization.ID)); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("let_managers_change_subscription", rawOrganization.IsEligibleUpdateSubscription); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("is_locked", rawOrganization.IsLocked); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("is_read_only", rawOrganization.IsReadOnly); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("name", rawOrganization.Name); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("partner_id", rawOrganization.PartnerID); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("partner_name", rawOrganization.PartnerName); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("phone", rawOrganization.Phone); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("projects", rawOrganization.Projects); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("servers", rawOrganization.Servers); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("users", rawOrganization.Users); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := data.Set("vat_number", rawOrganization.VatNumber); err != nil {
+		return diag.FromErr(err)
+	}
+
+	data.SetId(i32toa(id))
+
 	return nil
 }
 
