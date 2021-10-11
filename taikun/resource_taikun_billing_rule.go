695561549a15364c951a97e76279a1743db6b75d
Antoine <antoine.labarussias@epita.fr>
2021-10-11 09:53:12 +0200

    feat(billing_rules): implement resource update

--- taikun/resource_taikun_billing_rule.go
+++ taikun/resource_taikun_billing_rule.go
@@ -36,6 +36,7 @@ func resourceTaikunBillingRule() *schema.Resource {
 				Description: "Labels linked to this billing rule.",
 				Type:        schema.TypeList,
 				Required:    true,
+				ForceNew:    true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"label": {
@@ -198,7 +199,32 @@ func resourceTaikunBillingRuleRead(_ context.Context, data *schema.ResourceData,
 }
 
 func resourceTaikunBillingRuleUpdate(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	return nil
+	apiClient := meta.(*apiClient)
+	id, err := atoi32(data.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	//billingCredentialId, err := atoi32(data.Get("billing_credential_id").(string))
+	//if err != nil {
+	//	return diag.Errorf("billing_credential_id isn't valid: %s", data.Get("billing_credential_id").(string))
+	//}
+
+	body := &models.RuleForUpdateDto{
+		Name:       data.Get("name").(string),
+		MetricName: data.Get("metric_name").(string),
+		Price:      data.Get("price").(float64),
+		//OperationCredentialID: billingCredentialId,
+		Type: models.PrometheusType(getPrometheusType(data.Get("type").(string))),
+	}
+
+	params := prometheus.NewPrometheusUpdateParams().WithV(ApiVersion).WithID(id).WithBody(body)
+	_, err = apiClient.client.Prometheus.PrometheusUpdate(params, apiClient)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceTaikunBillingRuleRead(ctx, data, meta)
 }
 
 func resourceTaikunBillingRuleDelete(_ context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
