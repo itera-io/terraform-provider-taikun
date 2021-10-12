3eee7cab4695205a4c51edea53285db44e6735bc
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-12 14:06:16 +0200

    refactor(slack_config): extract datasource schema definition

--- taikun/data_source_taikun_slack_configuration.go
+++ taikun/data_source_taikun_slack_configuration.go
@@ -7,48 +7,52 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+func dataSourceTaikunSlackConfigurationSchema() map[string]*schema.Schema {
+	return map[string]*schema.Schema{
+		"channel": {
+			Description: "Slack channel for notifications",
+			Type:        schema.TypeString,
+			Computed:    true,
+		},
+		"id": {
+			Description:  "ID",
+			Type:         schema.TypeString,
+			Required:     true,
+			ValidateFunc: stringIsInt,
+		},
+		"name": {
+			Description: "Name",
+			Type:        schema.TypeString,
+			Computed:    true,
+		},
+		"organization_id": {
+			Description: "Organization ID",
+			Type:        schema.TypeString,
+			Computed:    true,
+		},
+		"organization_name": {
+			Description: "Organization Name",
+			Type:        schema.TypeString,
+			Computed:    true,
+		},
+		"type": {
+			Description: "Alert (receive only alert-type of notification) or General (receive all notifications)",
+			Type:        schema.TypeString,
+			Computed:    true,
+		},
+		"url": {
+			Description: "Webhook URL from Slack app",
+			Type:        schema.TypeString,
+			Computed:    true,
+		},
+	}
+}
+
 func dataSourceTaikunSlackConfiguration() *schema.Resource {
 	return &schema.Resource{
 		Description: "Get a slack configuration by its ID.",
 		ReadContext: dataSourceTaikunSlackConfigurationRead,
-		Schema: map[string]*schema.Schema{
-			"channel": {
-				Description: "Slack channel for notifications",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"id": {
-				Description:  "ID",
-				Type:         schema.TypeString,
-				Required:     true,
-				ValidateFunc: stringIsInt,
-			},
-			"name": {
-				Description: "Name",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"organization_id": {
-				Description: "Organization ID",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"organization_name": {
-				Description: "Organization Name",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"type": {
-				Description: "Alert (receive only alert-type of notification) or General (receive all notifications)",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-			"url": {
-				Description: "Webhook URL from Slack app",
-				Type:        schema.TypeString,
-				Computed:    true,
-			},
-		},
+		Schema:      dataSourceTaikunSlackConfigurationSchema(),
 	}
 }
 
