f52665d7bd75b36c4957ac2c43c92ca9fcbb3a46
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-13 11:19:54 +0200

    test(slack_configs): add test with filter

--- taikun/data_source_taikun_slack_configurations_test.go
+++ taikun/data_source_taikun_slack_configurations_test.go
@@ -32,6 +32,58 @@ func TestAccDataSourceTaikunSlackConfigurations(t *testing.T) {
 			{
 				Config: fmt.Sprintf(testAccDataSourceTaikunSlackConfigurationsConfig, slackConfigurationName),
 				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.taikun_slack_configurations.all", "id", "all"),
+					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "slack_configurations.#"),
+					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "slack_configurations.0.channel"),
+					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "slack_configurations.0.id"),
+					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "slack_configurations.0.organization_id"),
+					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "slack_configurations.0.organization_name"),
+					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "slack_configurations.0.name"),
+					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "slack_configurations.0.type"),
+					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "slack_configurations.0.url"),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataSourceTaikunSlackConfigurationsWithFilterConfig = `
+resource "taikun_organization" "foo" {
+  name = "%s"
+  full_name = "Foo"
+  discount_rate = 42
+}
+
+resource "taikun_slack_configuration" "foo" {
+  organization_id = resource.taikun_organization.foo.id
+
+  name = "%s"
+  url = "https://www.example.org"
+  channel = "any"
+  type = "General"
+}
+
+data "taikun_slack_configurations" "all" {
+  organization_id = resource.taikun_organization.foo.id
+
+  depends_on = [
+    taikun_slack_configuration.foo
+  ]
+}
+`
+
+func TestAccDataSourceTaikunSlackConfigurationsWithFilter(t *testing.T) {
+	organizationName := randomTestName()
+	slackConfigurationName := randomTestName()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testAccDataSourceTaikunSlackConfigurationsWithFilterConfig, organizationName, slackConfigurationName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "id"),
 					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "slack_configurations.#"),
 					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "slack_configurations.0.channel"),
 					resource.TestCheckResourceAttrSet("data.taikun_slack_configurations.all", "slack_configurations.0.id"),
