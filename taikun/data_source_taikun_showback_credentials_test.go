9f9615ffe594dc7bb93ee39899ac717ee393d54e
Antoine <antoine.labarussias@epita.fr>
2021-10-13 13:32:10 +0200

    test(showback_credentials): add datasource test with organization filter

--- taikun/data_source_taikun_showback_credentials_test.go
+++ taikun/data_source_taikun_showback_credentials_test.go
@@ -53,3 +53,60 @@ func TestAccDataSourceTaikunShowbackCredentials(t *testing.T) {
 		},
 	})
 }
+
+const testAccDataSourceTaikunShowbackCredentialsWithFilterConfig = `
+resource "taikun_organization" "foo" {
+  name = "%s"
+  full_name = "Foo"
+  discount_rate = 42
+}
+
+resource "taikun_showback_credential" "foo" {
+  name            = "%s"
+  organization_id = resource.taikun_organization.foo.id
+
+  password = "%s"
+  url = "%s"
+  username = "%s"
+}
+
+data "taikun_showback_credentials" "all" {
+  organization_id = resource.taikun_organization.foo.id
+
+  depends_on = [
+    taikun_showback_credential.foo
+  ]
+}`
+
+func TestAccDataSourceTaikunShowbackCredentialsWithFilter(t *testing.T) {
+	organizationName := randomTestName()
+	showbackCredentialName := randomTestName()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t); testAccPreCheckPrometheus(t) },
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testAccDataSourceTaikunShowbackCredentialsWithFilterConfig,
+					organizationName,
+					showbackCredentialName,
+					os.Getenv("PROMETHEUS_PASSWORD"),
+					os.Getenv("PROMETHEUS_URL"),
+					os.Getenv("PROMETHEUS_USERNAME"),
+				),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.taikun_showback_credentials.all", "showback_credentials.0.organization_name", organizationName),
+					resource.TestCheckResourceAttrSet("data.taikun_showback_credentials.all", "showback_credentials.#"),
+					resource.TestCheckResourceAttrSet("data.taikun_showback_credentials.all", "showback_credentials.0.created_by"),
+					resource.TestCheckResourceAttrSet("data.taikun_showback_credentials.all", "showback_credentials.0.id"),
+					resource.TestCheckResourceAttrSet("data.taikun_showback_credentials.all", "showback_credentials.0.is_locked"),
+					resource.TestCheckResourceAttrSet("data.taikun_showback_credentials.all", "showback_credentials.0.name"),
+					resource.TestCheckResourceAttrSet("data.taikun_showback_credentials.all", "showback_credentials.0.organization_id"),
+					resource.TestCheckResourceAttrSet("data.taikun_showback_credentials.all", "showback_credentials.0.password"),
+					resource.TestCheckResourceAttrSet("data.taikun_showback_credentials.all", "showback_credentials.0.url"),
+					resource.TestCheckResourceAttrSet("data.taikun_showback_credentials.all", "showback_credentials.0.username"),
+				),
+			},
+		},
+	})
+}
