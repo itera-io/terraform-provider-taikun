3eb3334c4cbba74d18c21c41adaf708f982fb8da
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-13 10:55:33 +0200

    test(billing_credentials): add test with filter

--- taikun/data_source_taikun_billing_credentials_test.go
+++ taikun/data_source_taikun_billing_credentials_test.go
@@ -55,3 +55,61 @@ func TestAccDataSourceTaikunBillingCredentials(t *testing.T) {
 		},
 	})
 }
+
+const testAccDataSourceTaikunBillingCredentialsWithFilterConfig = `
+resource "taikun_organization" "foo" {
+  name = "%s"
+  full_name = "Foo"
+  discount_rate = 42
+}
+
+resource "taikun_billing_credential" "foo" {
+  name = "%s"
+  organization_id = resource.taikun_organization.foo.id
+
+  prometheus_password = "%s"
+  prometheus_url      = "%s"
+  prometheus_username = "%s"
+}
+
+data "taikun_billing_credentials" "all" {
+  organization_id = resource.taikun_organization.foo.id
+
+   depends_on = [
+    taikun_billing_credential.foo
+  ]
+}`
+
+func TestAccDataSourceTaikunBillingCredentialsWithFilter(t *testing.T) {
+	billingCredentialName := randomTestName()
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t); testAccPreCheckPrometheus(t) },
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testAccDataSourceTaikunBillingCredentialsWithFilterConfig,
+					randomTestName(),
+					billingCredentialName,
+					os.Getenv("PROMETHEUS_PASSWORD"),
+					os.Getenv("PROMETHEUS_URL"),
+					os.Getenv("PROMETHEUS_USERNAME"),
+				),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "id"),
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "billing_credentials.#"),
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "billing_credentials.0.created_by"),
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "billing_credentials.0.id"),
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "billing_credentials.0.is_locked"),
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "billing_credentials.0.is_default"),
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "billing_credentials.0.name"),
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "billing_credentials.0.organization_id"),
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "billing_credentials.0.organization_name"),
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "billing_credentials.0.prometheus_password"),
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "billing_credentials.0.prometheus_url"),
+					resource.TestCheckResourceAttrSet("data.taikun_billing_credentials.all", "billing_credentials.0.prometheus_username"),
+				),
+			},
+		},
+	})
+}
