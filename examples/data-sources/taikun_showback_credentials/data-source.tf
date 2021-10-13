114a14f433ae5cd8fa0dc94bdf2fab8b0416616a
Antoine <antoine.labarussias@epita.fr>
2021-10-13 13:41:37 +0200

    docs: update generated docs

---
+++ examples/data-sources/taikun_showback_credentials/data-source.tf
@@ -0,0 +1,4 @@
+data "taikun_showback_credentials" "foo" {
+  # Optional for Partner and Admin
+  organization_id = "42"
+}
---
+++ examples/resources/taikun_showback_credential/import.sh
@@ -0,0 +1,2 @@
+# import with Taikun ID
+terraform import taikun_showback_credential.mycredential 42
