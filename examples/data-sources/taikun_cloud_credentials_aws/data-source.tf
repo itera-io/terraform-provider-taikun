6592c0a4c67a491176444253e6250dc08a3296bd
Antoine <antoine.labarussias@epita.fr>
2021-10-18 16:42:19 +0200

    docs: update generated docs

---
+++ examples/data-sources/taikun_cloud_credentials_aws/data-source.tf
@@ -0,0 +1,4 @@
+data "taikun_cloud_credentials_aws" "foo" {
+  # Optional
+  organization_id = "42"
+}
---
+++ examples/data-sources/taikun_cloud_credentials_azure/data-source.tf
@@ -0,0 +1,4 @@
+data "taikun_cloud_credentials_azure" "foo" {
+  # Optional
+  organization_id = "42"
+}
