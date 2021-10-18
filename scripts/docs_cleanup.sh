1e5044be17247ec445312ba2108294dfbd4ffa19
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-18 13:35:40 +0200

    docs: edit proofreading script's messages

--- scripts/docs_cleanup.sh
+++ scripts/docs_cleanup.sh
@@ -3,7 +3,7 @@
 set -e
 
 remove_optional_id_from_datasources_doc() {
-  echo "Removing optional ID attribute from list-type datasources' documentation..."
+  echo "removing optional id attribute from list-type datasources' documentation"
   target_files=$(ls docs/data-sources/*s.md)
   offending_line="- \*\*id\*\* (String) The ID of this resource."
   modified_files=0
@@ -11,6 +11,7 @@ remove_optional_id_from_datasources_doc() {
     if ! grep -- "${offending_line}" "${target_file}" &>/dev/null; then
       continue
     fi
+    echo "correcting \"${target_file}\""
     modified_files=$((${modified_files}+1))
     sed -i "/${offending_line}/d" "${target_file}"
     optional_header_line=$(grep -n '### Optional' "${target_file}" | cut -d ':' -f 1)
@@ -18,9 +19,8 @@ remove_optional_id_from_datasources_doc() {
     if ! (sed -n "${optional_header_line},${read_only_header_line}p" "${target_file}" | fgrep -- '- **' &>/dev/null); then
       sed -i "${optional_header_line},$((${read_only_header_line}-1))d" "${target_file}"
     fi
-    echo -e "  - Modified ${target_file}"
   done
-  echo "Done. Modified ${modified_files} files."
+  echo "corrected ${modified_files} files"
 }
 
 remove_optional_id_from_datasources_doc
