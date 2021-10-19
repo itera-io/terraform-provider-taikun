afd542ee58a72cbb4f2cc0773adb61bf9a6eb234
Antoine <antoine.labarussias@epita.fr>
2021-10-19 13:28:55 +0200

    feat(provider): add api_host parameter

--- taikun/provider.go
+++ taikun/provider.go
@@ -107,6 +107,13 @@ func Provider() *schema.Provider {
 			"taikun_user":                                 resourceTaikunUser(),
 		},
 		Schema: map[string]*schema.Schema{
+			"api_host": {
+				Type:         schema.TypeString,
+				Description:  "Taikun API host.",
+				Optional:     true,
+				Default:      "api.taikun.dev",
+				ValidateFunc: validation.StringIsNotEmpty,
+			},
 			"email": {
 				Type:          schema.TypeString,
 				Description:   "Taikun email.",
@@ -164,8 +171,14 @@ func configureContextFunc(_ context.Context, data *schema.ResourceData) (interfa
 		return nil, diag.Errorf("You must define an email and a password")
 	}
 
+	transportConfig := &client.TransportConfig{
+		Host:     data.Get("api_host").(string),
+		BasePath: client.DefaultBasePath,
+		Schemes:  client.DefaultSchemes,
+	}
+
 	return &apiClient{
-		client:              client.Default,
+		client:              client.NewHTTPClientWithConfig(nil, transportConfig),
 		email:               email.(string),
 		password:            password.(string),
 		useKeycloakEndpoint: keycloakEnabled,
