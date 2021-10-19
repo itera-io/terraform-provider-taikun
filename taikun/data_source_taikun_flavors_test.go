a7ce3aeda6027d1b18d0e7760d43454584c5d961
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-19 10:35:56 +0200

    test(flavors): add azure flavors test

--- taikun/data_source_taikun_flavors_test.go
+++ taikun/data_source_taikun_flavors_test.go
@@ -51,3 +51,49 @@ func TestAccDataSourceTaikunFlavorsAWS(t *testing.T) {
 		},
 	})
 }
+
+const testAccDataSourceTaikunFlavorsAzureConfig = `
+resource "taikun_cloud_credential_azure" "foo" {
+  name = "%s"
+  availability_zone = "%s"
+  location = "%s"
+}
+
+data "taikun_flavors" "foo" {
+  cloud_type = "Azure"
+  cloud_credential_id = resource.taikun_cloud_credential_azure.foo.id
+
+  min_cpu = %d
+  max_cpu = %d
+  min_ram = %d
+  max_ram = %d
+}
+`
+
+func TestAccDataSourceTaikunFlavorsAzure(t *testing.T) {
+	cloudCredentialName := randomTestName()
+	cpu := 16
+	ram := 64
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t); testAccPreCheckAzure(t) },
+		ProviderFactories: testAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testAccDataSourceTaikunFlavorsAzureConfig,
+					cloudCredentialName,
+					os.Getenv("ARM_AVAILABILITY_ZONE"),
+					os.Getenv("ARM_LOCATION"),
+					cpu, cpu,
+					ram, ram,
+				),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet("data.taikun_flavors.foo", "flavors.#"),
+					resource.TestCheckResourceAttrSet("data.taikun_flavors.foo", "flavors.0.name"),
+					resource.TestCheckResourceAttr("data.taikun_flavors.foo", "flavors.0.cpu", fmt.Sprint(cpu)),
+					resource.TestCheckResourceAttr("data.taikun_flavors.foo", "flavors.0.ram", fmt.Sprint(ram)),
+				),
+			},
+		},
+	})
+}
