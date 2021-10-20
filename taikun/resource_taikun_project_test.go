d3ce8779737d8f248b53c9c8f4e8a2ae2c592a94
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-20 15:56:02 +0200

    test(project): add basic acceptance test

---
+++ taikun/resource_taikun_project_test.go
@@ -0,0 +1,140 @@
+package taikun
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/itera-io/taikungoclient/client/projects"
+	"github.com/itera-io/taikungoclient/models"
+)
+
+func init() {
+	resource.AddTestSweepers("taikun_project", &resource.Sweeper{
+		Name:         "taikun_project",
+		Dependencies: []string{},
+		F: func(r string) error {
+
+			meta, err := sharedConfig()
+			if err != nil {
+				return err
+			}
+			apiClient := meta.(*apiClient)
+
+			params := projects.NewProjectsListParams().WithV(ApiVersion)
+
+			var projectList []*models.ProjectListForUIDto
+
+			for {
+				response, err := apiClient.client.Projects.ProjectsList(params, apiClient)
+				if err != nil {
+					return err
+				}
+				projectList = append(projectList, response.GetPayload().Data...)
+				if len(projectList) == int(response.GetPayload().TotalCount) {
+					break
+				}
+				offset := int32(len(projectList))
+				params = params.WithOffset(&offset)
+			}
+
+			for _, e := range projectList {
+				if strings.HasPrefix(e.Name, testNamePrefix) {
+					params := projects.NewProjectsDeleteParams().WithV(ApiVersion).WithBody(&models.DeleteProjectCommand{ProjectID: e.ID})
+					_, _, err = apiClient.client.Projects.ProjectsDelete(params, apiClient)
+					if err != nil {
+						return err
+					}
+				}
+			}
+
+			return nil
+		},
+	})
+}
+
+const testAccResourceTaikunProjectConfig = `
+resource "taikun_cloud_credential_aws" "foo" {
+  name = "%s"
+  availability_zone = "%s"
+}
+
+resource "taikun_project" "foo" {
+  name = "%s"
+  cloud_credential_id = resource.taikun_cloud_credential_aws.foo.id
+}
+`
+
+func TestAccResourceTaikunProject(t *testing.T) {
+	cloudCredentialName := randomTestName()
+	projectName := randomTestName()
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t); testAccPreCheckAWS(t) },
+		ProviderFactories: testAccProviderFactories,
+		CheckDestroy:      testAccCheckTaikunProjectDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testAccResourceTaikunProjectConfig,
+					cloudCredentialName,
+					os.Getenv("AWS_AVAILABILITY_ZONE"),
+					projectName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckTaikunProjectExists,
+					resource.TestCheckResourceAttr("taikun_project.foo", "name", projectName),
+					resource.TestCheckResourceAttrSet("taikun_project.foo", "access_profile_id"),
+					resource.TestCheckResourceAttrSet("taikun_project.foo", "alerting_profile_id"),
+					resource.TestCheckResourceAttrSet("taikun_project.foo", "cloud_credential_id"),
+					resource.TestCheckResourceAttrSet("taikun_project.foo", "kubernetes_profile_id"),
+					resource.TestCheckResourceAttrSet("taikun_project.foo", "project_id"),
+				),
+			},
+			{
+				ResourceName:      "taikun_project.foo",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+		},
+	})
+}
+
+func testAccCheckTaikunProjectExists(state *terraform.State) error {
+	apiClient := testAccProvider.Meta().(*apiClient)
+
+	for _, rs := range state.RootModule().Resources {
+		if rs.Type != "taikun_project" {
+			continue
+		}
+
+		params := projects.NewProjectsListParams().WithV(ApiVersion).WithSearchID(&rs.Primary.ID)
+
+		response, err := apiClient.client.Projects.ProjectsList(params, apiClient)
+		if err != nil || len(response.Payload.Data) != 1 {
+			return fmt.Errorf("project doesn't exist (id = %s)", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckTaikunProjectDestroy(state *terraform.State) error {
+	apiClient := testAccProvider.Meta().(*apiClient)
+
+	for _, rs := range state.RootModule().Resources {
+		if rs.Type != "taikun_project" {
+			continue
+		}
+
+		params := projects.NewProjectsListParams().WithV(ApiVersion).WithSearchID(&rs.Primary.ID)
+
+		response, err := apiClient.client.Projects.ProjectsList(params, apiClient)
+		if err == nil && response.Payload.TotalCount != 0 {
+			return fmt.Errorf("project still exists (id = %s)", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
