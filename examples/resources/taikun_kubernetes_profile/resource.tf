44d51ea6276ca5bf1d9efa67a6c2bc6839911df6
Antoine <antoine.labarussias@epita.fr>
2021-10-21 11:06:23 +0200

    docs(kubernetes_profiles): update example

--- examples/resources/taikun_kubernetes_profile/resource.tf
+++ examples/resources/taikun_kubernetes_profile/resource.tf
@@ -4,5 +4,6 @@ resource "taikun_kubernetes_profile" "foo" {
   organization_id         = "42"
   load_balancing_solution = "Taikun"
   bastion_proxy_enabled   = true
+  schedule_on_master      = false
   is_locked               = true
 }
