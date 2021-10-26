fe92c2ed78d63cf36e274fb42fc2abe648c893e2
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2021-10-26 15:50:13 +0200

    refactor(retries): refactor read after create/update

--- taikun/waiter.go
+++ taikun/waiter.go
@@ -9,10 +9,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-const (
-	readAfterCreateOperationTimeout = 2 * time.Minute
-	readAfterUpdateOperationTimeout = 1 * time.Minute
-)
+func getReadAfterOpTimeout(isUpdate bool) time.Duration {
+	if isUpdate {
+		return 1 * time.Minute
+	}
+	return 2 * time.Minute
+}
 
 func timedOut(err error) bool {
 	timeoutErr, ok := err.(*resource.TimeoutError)
@@ -22,35 +24,31 @@ func timedOut(err error) bool {
 type readFunc func(context.Context, *schema.ResourceData, interface{}) diag.Diagnostics
 
 func readAfterCreateWithRetries(readFunc readFunc, ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	err := resource.RetryContext(ctx, readAfterCreateOperationTimeout, func() *resource.RetryError {
-		readErr := readFunc(ctx, data, meta)
-		if readErr != nil {
-			return resource.NonRetryableError(nil)
-		}
-		if data.Id() == "" {
-			return resource.RetryableError(nil)
-		}
-		return nil
-	})
-	if timedOut(err) {
-		return diag.Errorf("timed out reading newly created resource")
-	}
-	return nil
+	isUpdate := false
+	return readAfterOpWithRetries(readFunc, ctx, data, meta, isUpdate)
 }
 
 func readAfterUpdateWithRetries(readFunc readFunc, ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	err := resource.RetryContext(ctx, readAfterUpdateOperationTimeout, func() *resource.RetryError {
+	isUpdate := true
+	return readAfterOpWithRetries(readFunc, ctx, data, meta, isUpdate)
+}
+
+func readAfterOpWithRetries(readFunc readFunc, ctx context.Context, data *schema.ResourceData, meta interface{}, isUpdate bool) diag.Diagnostics {
+	retryErr := resource.RetryContext(ctx, getReadAfterOpTimeout(isUpdate), func() *resource.RetryError {
 		readErr := readFunc(ctx, data, meta)
 		if readErr != nil {
-			return resource.NonRetryableError(nil)
+			return resource.NonRetryableError(nil) // FIXME pass read error
 		}
 		if data.Id() == "" {
 			return resource.RetryableError(nil)
 		}
 		return nil
 	})
-	if timedOut(err) {
-		return diag.Errorf("timed out reading newly updated resource")
+	if timedOut(retryErr) {
+		if isUpdate {
+			return diag.Errorf("timed out reading newly updated resource")
+		}
+		return diag.Errorf("timed out reading newly created resource")
 	}
-	return nil
+	return nil // FIXME pass read error
 }
