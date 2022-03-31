7b582b0e442b0f4b077868c436407e76ca84327a
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-03-31 13:33:58 +0200

    feat(gcp): implement credentials data source read

--- taikun/data_source_taikun_cloud_credentials_gcp.go
+++ taikun/data_source_taikun_cloud_credentials_gcp.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/itera-io/taikungoclient/client/cloud_credentials"
+	"github.com/itera-io/taikungoclient/models"
 )
 
 func dataSourceTaikunCloudCredentialsGCP() *schema.Resource {
@@ -31,6 +33,44 @@ func dataSourceTaikunCloudCredentialsGCP() *schema.Resource {
 }
 
 func dataSourceTaikunCloudCredentialsGCPRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// FIXME
+	apiClient := meta.(*apiClient)
+	dataSourceID := "all"
+
+	params := cloud_credentials.NewCloudCredentialsDashboardListParams().WithV(ApiVersion)
+
+	organizationIDData, organizationIDProvided := d.GetOk("organization_id")
+	if organizationIDProvided {
+		dataSourceID = organizationIDData.(string)
+		organizationID, err := atoi32(dataSourceID)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		params = params.WithOrganizationID(&organizationID)
+	}
+
+	var cloudCredentialsList []*models.GoogleCredentialsListDto
+	for {
+		response, err := apiClient.client.CloudCredentials.CloudCredentialsDashboardList(params, apiClient)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+		cloudCredentialsList = append(cloudCredentialsList, response.GetPayload().Google...)
+		if len(cloudCredentialsList) == int(response.GetPayload().TotalCountGoogle) {
+			break
+		}
+		offset := int32(len(cloudCredentialsList))
+		params = params.WithOffset(&offset)
+	}
+
+	cloudCredentials := make([]map[string]interface{}, len(cloudCredentialsList))
+	for i, rawCloudCredential := range cloudCredentialsList {
+		cloudCredentials[i] = flattenTaikunCloudCredentialGCP(rawCloudCredential)
+	}
+	if err := d.Set("cloud_credentials", cloudCredentials); err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(dataSourceID)
+
 	return nil
 }
