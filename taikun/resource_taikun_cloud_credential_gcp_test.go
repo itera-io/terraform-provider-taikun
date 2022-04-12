8b6bd8f247fbf18cfedb9306f5ca997ad05d8bda
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-04-12 10:49:54 +0200

    test(gcp): implement resource exists and destroyed tests

--- taikun/resource_taikun_cloud_credential_gcp_test.go
+++ taikun/resource_taikun_cloud_credential_gcp_test.go
@@ -1,12 +1,15 @@
 package taikun
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/itera-io/taikungoclient/client/cloud_credentials"
 )
 
 const testAccResourceTaikunCloudCredentialGCPConfig = `
@@ -92,13 +95,53 @@ func TestAccResourceTaikunCloudCredentialGCPImportProject(t *testing.T) {
 }
 
 func testAccCheckTaikunCloudCredentialGCPExists(state *terraform.State) error {
-	// FIXME
+	client := testAccProvider.Meta().(*apiClient)
+
+	for _, rs := range state.RootModule().Resources {
+		if rs.Type != "taikun_cloud_credential_gcp" {
+			continue
+		}
+
+		id, _ := atoi32(rs.Primary.ID)
+		params := cloud_credentials.NewCloudCredentialsDashboardListParams().WithV(ApiVersion).WithID(&id)
+
+		response, err := client.client.CloudCredentials.CloudCredentialsDashboardList(params, client)
+		if err != nil || response.Payload.TotalCountGoogle != 1 {
+			return fmt.Errorf("gcp cloud credential doesn't exist (id = %s)", rs.Primary.ID)
+		}
+	}
 
 	return nil
 }
 
 func testAccCheckTaikunCloudCredentialGCPDestroy(state *terraform.State) error {
-	// FIXME
+	client := testAccProvider.Meta().(*apiClient)
+
+	for _, rs := range state.RootModule().Resources {
+		if rs.Type != "taikun_cloud_credential_gcp" {
+			continue
+		}
+
+		retryErr := resource.RetryContext(context.Background(), getReadAfterOpTimeout(false), func() *resource.RetryError {
+			id, _ := atoi32(rs.Primary.ID)
+			params := cloud_credentials.NewCloudCredentialsDashboardListParams().WithV(ApiVersion).WithID(&id)
+
+			response, err := client.client.CloudCredentials.CloudCredentialsDashboardList(params, client)
+			if err != nil {
+				return resource.NonRetryableError(err)
+			}
+			if response.Payload.TotalCountGoogle != 0 {
+				return resource.RetryableError(errors.New("gcp cloud credential still exists"))
+			}
+			return nil
+		})
+		if timedOut(retryErr) {
+			return errors.New("gcp cloud credential still exists (timed out)")
+		}
+		if retryErr != nil {
+			return retryErr
+		}
+	}
 
 	return nil
 }
