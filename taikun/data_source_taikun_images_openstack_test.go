8aedcfeba55f1014c4423c5db1fecf6fdcaf160c
Thibaud Deleon <91742111+tdel0@users.noreply.github.com>
2022-09-05 04:17:51 +0200

    test(images_openstack): fix config

--- taikun/data_source_taikun_images_openstack_test.go
+++ taikun/data_source_taikun_images_openstack_test.go
@@ -12,7 +12,7 @@ resource "taikun_cloud_credential_openstack" "foo" {
   name = "%s"
 }
 
-data "taikun_images" "foo" {
+data "taikun_images_openstack" "foo" {
   cloud_credential_id = resource.taikun_cloud_credential_openstack.foo.id
 }
 `
