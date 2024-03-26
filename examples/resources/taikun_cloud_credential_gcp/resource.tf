c3c8f600dba3885ef84af8e8a480f2a525f88f69
Radek Smid <radek.smid@taikun.cloud>
2024-03-26 17:30:29 +0100

    chore: Documentation update.

--- examples/resources/taikun_cloud_credential_gcp/resource.tf
+++ examples/resources/taikun_cloud_credential_gcp/resource.tf
@@ -1,24 +1,27 @@
 resource "taikun_cloud_credential_gcp" "foo" {
-  name               = "foo"
-  config_file        = "./gcp.json"
-  billing_account_id = "000000000000"
-  folder_id          = "000000-000000-000000"
-  region             = "asia-northeast1"
-  zone               = "asia-northeast1-b"
+  name = "foo"
+
+  config_file    = "./config.json"
+  region         = "europe-north1"
+  import_project = true
+
+  organization_id = "42"
+  lock            = false
 }
 
 data "taikun_images_gcp" "foo" {
   cloud_credential_id = resource.taikun_cloud_credential_gcp.foo.id
-  type                = "windows"
+  type                = "ubuntu"
+  latest              = true
 }
 
 locals {
   images = [for image in data.taikun_images_gcp.foo.images : image.name] // GCP uses image names, not image ids.
 }
 
+
 resource "taikun_project" "foo" {
   name                = "mock-project"
   cloud_credential_id = resource.taikun_cloud_credential_gcp.foo.id
-  flavors             = local.flavors
   images              = local.images
 }
